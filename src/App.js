391d3695c14f2e34b922d66a08f86961ce7c441a
michaelc511 <michaelc511@gmail.com>
2023-12-25 19:11:58 -0800

    4th

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog'
+  const likes = 50
+  const person = { name: 'Yoshi', age: 30 }
+  const link = 'http://www.google.com'
   return (
-    <div className="App">
-      <div className="content" >
-        <h1>App Component</h1>
+    <div className='App'>
+      <div className='content' >
+        <h1>{title}</h1>
+        <p>Liked {likes} times </p>
+        <p>{person.name}</p>
+
+        <p> {10}</p>
+        <p>{"Hello there"}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 10}</p>
+        <a href={link}>Google</a>
       </div>
     </div>
   );
