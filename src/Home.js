36124965bb3ea373462c4787fa57e7a728f5cdd4
michaelc511 <michaelc511@gmail.com>
2023-12-26 09:54:00 -0800

    7_click_events

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,44 @@
 const Home = () => {
-  return ( 
+
+  const handleClick0 = () => {
+    console.log('hello ninjas');
+  }
+
+
+  const handleClick = (e) => {
+    console.log('hello ninjas', e);
+  }
+
+  // pass an argument. 
+  const handleClickAgain = (name, e) => {
+    console.log('hello ' + name, e.target);
+  }
+
+  // pass an argument w event in 1st arg
+  const handleClickAgain2 = (e, name) => {
+    console.log('hello ' + name, e.target);
+  }
+
+  return (
     <div className="home">
       <h2>Homepage</h2>
-    </div>
 
-   );
+      {/* Passing in a reference to the function 
+          Do not run it with functionName()
+          
+          Event 'e' is optional if no parameters
+
+          For passing in an argument, need to wrap it in an
+          anonymous function
+      */}
+
+      <button onClick={handleClick0}>Click m0</button>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
+      <button onClick={(e) => handleClickAgain2(e, 'mario')}>Click me again</button>
+
+    </div>
+  );
 }
- 
+
 export default Home;
