6643f5936b926d0221a729877a17aeaa71f64fd8
michaelc511 <michaelc511@gmail.com>
2023-12-25 19:40:01 -0800

    5th

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import NavBar from './Navbar';
+import Home from './Home';
 
 function App() {
   const title = 'Welcome to the new blog'
@@ -7,16 +9,9 @@ function App() {
   const link = 'http://www.google.com'
   return (
     <div className='App'>
-      <div className='content' >
-        <h1>{title}</h1>
-        <p>Liked {likes} times </p>
-        <p>{person.name}</p>
-
-        <p> {10}</p>
-        <p>{"Hello there"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-        <a href={link}>Google</a>
+      <NavBar></NavBar>
+      <div className="content">
+        <Home/>
       </div>
     </div>
   );
