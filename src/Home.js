d932f4aedd03db79641dd6538a905095e8db642d
michaelc511 <michaelc511@gmail.com>
2023-12-26 17:28:10 -0800

    10 solution

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,39 @@
 import { useState } from 'react';
 
 const Home = () => {
+  // an array of blogs for our state
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
-  let firstName = 'mario';
-  // Does not change 'firstName' in web template but in console.log
-  // React is not reactive 'name' to rerender it on the web
-  const handleClick = () => {
-    firstName = 'luigi'
-    console.log(firstName);
-  }
+  /*
+    We need a template for all the items in the array.
+    Instead of hard-coding, we should not repeat and be able to access new data.
+    We need to cycle through the array and for each of them, create a template.
+    Use the 'map' method to do something in each array. 
 
-  // Need to use hook 'useState' to have React track it
-  const [name, setName] = useState('mario')
-  const [age, setAge] = useState(25);
-
-  const handleClick1 = () => {
-    setName('luigi');
-    setAge(30);
-  }
+    We need unique 'key' values from the blog.id 
+  */
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{name} is {age} years old </p>
-      <button onClick={handleClick}>Click me will not work</button>
-      <p>       
-        <button onClick={handleClick1}>Click me will work using 'useState' hook</button>
-      </p>
-
-
+      {
+      // shorter way to do it. 
+      /* {blogs.map((blog) =>(
+        <div className = "blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by { blog.author} </p>
+        </div>
+      ))} */}
+      
+      {blogs.map((blog) => {
+        return (<div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author} </p>
+        </div>);
+      })}
     </div>
   );
 }
