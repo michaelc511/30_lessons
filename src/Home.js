d9a302475b2042b145d596dea7412dd340bee44f
michaelc511 <michaelc511@gmail.com>
2023-12-28 15:36:01 -0800

    18

--- src/Home.js
+++ src/Home.js
@@ -2,30 +2,35 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  //1. set the useState to null for blogs
   // an array of blogs for our state
   const [blogs, setBlogs] = useState(null);
 
-  const [name, setName] = useState('mario');
+  // 1. add a pending state boolean
+  const [isPending, setIsPending] = useState(true);
 
-  // useEffect - pass a function as an argument to run everytime we render and rerender  
-  // 2. fetch from our JSON server using .then for the response and a .then for the data
+  // useEffect - pass a function as an argument to run everytime we render and rerender   
   useEffect(() => {
     console.log('use effect run');
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then((data)=>{
-        console.log(data)
-        setBlogs(data)
-      });
+    // 4. have a setTimeout to load the data in 1 sec
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setBlogs(data);
+          // 3. set pending to false if loaded
+          setIsPending(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
-      {/* 3. Conditional templating - Add a condition to show it if blogs is not null */}
-      {blogs && <BlogList blogs={blogs} title="All Blogs" /> }
+      {/* 2. Conditional templating - If pending is true, show loading message */}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 }
