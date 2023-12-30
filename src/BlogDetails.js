52a682591fd1e94bfc1bf96cdf5d2ac338ac38fd
michaelc511 <michaelc511@gmail.com>
2023-12-30 15:55:18 -0800

    31

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,23 @@
-import { useParams } from "react-router-dom";
-import useFetch from './useFetch';
+import { useHistory, useParams } from "react-router-dom";
+import useFetch from './useFetch'; 
 
 const BlogDetails = () => {
   const { id } = useParams();
   const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+
+  // 3. History
+  const history = useHistory();
+
+  // 2. handleClick w fetch
+  const handleClick = () => {
+    fetch('http://localhost:8000/blogs/' + blog.id, {
+      method: 'DELETE'
+    })
+      .then(() => {
+        history.push('/')
+      })
+
+  }
 
   return (
     <div className="blog-details">
@@ -14,6 +28,8 @@ const BlogDetails = () => {
           <h2>{blog.title} </h2>
           <p>Writtey by {blog.author}</p>
           <div>{blog.body}</div>
+          {/* 1. add a delete button w onClick to delete it */}
+          <button onClick={handleClick}>Delete blog</button>
         </article>
       }
     </div>
--- src/Create.js
+++ src/Create.js
@@ -1,38 +1,33 @@
 import { useState } from 'react';
-// 1. import useHistory
 import { useHistory } from 'react-router-dom';
 
 const Create = () => {
   const [title, setTitle] = useState('Enter the title');
   const [body, setBody] = useState('Enter the body');
   const [author, setAuthor] = useState('mario');
-  const [ isPending, setIsPending ] = useState(false);
-  // 2. create an instanct
+  const [isPending, setIsPending] = useState(false);
   const history = useHistory();
- 
-  const handleSubmit = (e) => { 
-    e.preventDefault(); 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
     const blog = {
       title,
-       body,
-       author
+      body,
+      author
     }
- 
+
     setIsPending(true);
- 
+
     fetch('http://localhost:8000/blogs', {
       method: 'POST',
-      headers: { "Content-Type": "application/json"},
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(blog)
     })
-    .then(()=> {
-      console.log('new blog added'); 
-      setIsPending(false);
-      // go back to previous page. 
-      // history.go(-1);
-      // 3. go to home page
-      history.push('/');
-    })
+      .then(() => {
+        console.log('new blog added');
+        setIsPending(false);
+        history.push('/');
+      })
   }
 
   return (
@@ -60,9 +55,9 @@ const Create = () => {
           <option value="yoshi">yoshi</option>
         </select>
         {/* 5. Have conditional templating for not pending and pending to disable it */}
-        {!isPending && <button>Add Blog</button> }
+        {!isPending && <button>Add Blog</button>}
         {isPending && <button disabled>Adding Blog...</button>}
-        
+
       </form>
     </div>
   );
