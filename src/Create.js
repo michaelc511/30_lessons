c41943f680f2b80cf4558e692dc26fd3481d346a
michaelc511 <michaelc511@gmail.com>
2023-12-30 15:09:15 -0800

    28

--- src/Create.js
+++ src/Create.js
@@ -1,33 +1,42 @@
 import { useState } from 'react';
 
 const Create = () => {
-  // 2 create states for title and use it in the title field
   const [title, setTitle] = useState('Enter the title');
-  const [body, setBody ] = useState('Enter the body');
-  const [author, setAuthor ] = useState('mario');
+  const [body, setBody] = useState('Enter the body');
+  const [author, setAuthor] = useState('mario');
+
+  // 1. event for submit
+  const handleSubmit = (e) => {
+    // 2. prevent refresh of the page 
+    e.preventDefault();
+    // 3. Create an object for the blog w the state properties
+    const blog = {
+      title,
+       body,
+       author
+    }
+
+    console.log(blog);
+  }
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      {/* 1. Create the form  */}
-      <form >
+      <form onSubmit={handleSubmit}>
         <label >Blog Title</label>
         <input
           type="text"
           required
-          // 3. title value and onChange event
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
         <label >Blog Body</label>
-        <textarea 
+        <textarea
           required
-          // 4. body value and onChange event
           value={body}
           onChange={(e) => setBody(e.target.value)}></textarea>
         <label htmlFor="">Author</label>
-        <select 
-          // 5. author value and onChange event
+        <select
           value={author}
           onChange={(e) => setAuthor(e.target.value)}
         >
@@ -35,9 +44,7 @@ const Create = () => {
           <option value="yoshi">yoshi</option>
         </select>
         <button>Add Blog</button>
-        <p>{title}</p>
-        <p>{body}</p>
-        <p>{author}</p>
+
       </form>
     </div>
   );
