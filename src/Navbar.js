265272bb695c67bc467e013fea10bd34a129b8b0
michaelc511 <michaelc511@gmail.com>
2023-12-30 13:52:20 -0800

    24

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,10 @@
-// 1 import Link
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>The Dojo Blog</h1>
-      <div className="links"></div>
-      {/* // 2 use the Link here  */}
+      <div className="links"></div> 
       <Link to = "/">Home</Link>
       <Link to="/create">New Blog</Link>
         
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,13 +1,17 @@
-import { useState, useEffect } from 'react'; 
+import { useState, useEffect } from 'react';
 
-const useFetch = (url) => { 
-    const [data, setData] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error , setError ] = useState(null);
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    // 1 abortController
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      // 2 add 2nd parameter 
+      fetch(url, { signal: abortCont.signal })
         .then(res => {
           console.log(res);
           if (!res.ok) {
@@ -22,11 +26,20 @@ const useFetch = (url) => {
           setError(null);
         })
         .catch(err => {
-          console.log("ERROR: " + err.message);
-          setError(err.message);
-          setIsPending(false);
+          // 4 catch the abort error
+          if (err.name === 'AbortError') {
+            console.log('fetch aborted');
+          } else {
+            console.log("ERROR: " + err.message);
+            setError(err.message);
+            setIsPending(false);
+          }
         });
     }, 1000);
+
+    // 3 add it here to pause it
+    return () => abortCont.abort(); //console.log('cleanup');
+
   }, [url]);
 
   return { data, isPending, error }
