1d26874d3276618b2e9da06829645289a306d42e
michaelc511 <michaelc511@gmail.com>
2024-01-09 15:49:19 -0800

    arr passed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  let numbers = [1,2,3]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Child name="Mike" numbers={numbers}/>
     </div>
   );
 }
+
+export function Child({name, numbers}) {
+  return (
+  <div>
+    <h1>Hi {name}</h1>
+    {/* loop through the numbers using map */}
+    {numbers.map((num) =>{
+      return(
+        <div key={num}>{num}</div>
+      );
+    })}
+
+  </div>
+  )
+}
+
 
 export default App;
